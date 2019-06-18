c3d4295834909ce776d560087f84c81a4f084dad
vle <vle@sitecore.net>
2019-06-18 11:43:09 +0300

    fix

---
+++ src/Sitecore.Support.335596/XA/Foundation/Presentation/Pipelines/BuildPageDefinition/GetPlaceholderDefinitions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Sitecore.Data;
+using Sitecore.DependencyInjection;
+using Sitecore.Layouts;
+using Sitecore.Mvc.Pipelines.Response.BuildPageDefinition;
+using Sitecore.XA.Foundation.Abstractions;
+using Sitecore.XA.Foundation.Presentation;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.XA.Foundation.Multisite.Extensions;
+
+namespace Sitecore.Support.XA.Foundation.Presentation.Pipelines.BuildPageDefinition
+{
+    public class GetPlaceholderDefinitions : Sitecore.XA.Foundation.Presentation.Pipelines.BuildPageDefinition.GetPlaceholderDefinitions
+    {
+        public override void Process(BuildPageDefinitionArgs args)
+        {
+            if (!ServiceLocator.ServiceProvider.GetService<IContext>().Site.IsSxaSite())
+            {
+                return;
+            }
+
+            var placeholderPageDefinition = new PlaceholderPageDefinition(args.Result);
+            AddPlaceholders(placeholderPageDefinition, args);
+            args.Result = placeholderPageDefinition;
+        }
+
+        protected override void AddPlaceholders(PlaceholderPageDefinition definition, BuildPageDefinitionArgs args)
+        {
+            #region Fix 335956
+            // Get layout definition to reuse it
+            object layoutDefinition;
+            args.CustomData.TryGetValue("xmlBasedLayoutDefinition", out layoutDefinition);
+            if (layoutDefinition == null)
+            {
+                return;
+            }
+            #endregion
+
+            Dictionary<ID, IEnumerable<PlaceholderDefinition>> placeholders = GetPlaceholders(layoutDefinition as XElement, args);
+            definition.Placeholders = placeholders;
+        }
+    }
+}
---
+++ src/Sitecore.Support.335596/XA/Foundation/Presentation/Pipelines/BuildPageDefinition/ProcessXmlBasedLayoutDefinition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Pipelines;
+using Sitecore.Mvc.Pipelines.Response.BuildPageDefinition;
+using Sitecore.Mvc.Pipelines.Response.GetXmlBasedLayoutDefinition;
+using Sitecore.Mvc.Presentation;
+
+namespace Sitecore.Support.XA.Foundation.Presentation.Pipelines.BuildPageDefinition
+{
+    public class ProcessXmlBasedLayoutDefinition : Sitecore.Mvc.Pipelines.Response.BuildPageDefinition.ProcessXmlBasedLayoutDefinition
+    {
+        protected override void AddRenderings(PageDefinition pageDefinition, BuildPageDefinitionArgs args)
+        {
+            XElement layoutDefinition = PipelineService.Get().RunPipeline<GetXmlBasedLayoutDefinitionArgs, XElement>("mvc.getXmlBasedLayoutDefinition", new GetXmlBasedLayoutDefinitionArgs(), a => a.Result);
+            if (layoutDefinition == null)
+            {
+                return;
+            }
+
+            #region Fix 335596
+            // Save layout definition to reuse it later
+            args.CustomData.Add("xmlBasedLayoutDefinition", layoutDefinition);
+            #endregion
+
+            IEnumerable<Rendering> renderings = GetRenderings(layoutDefinition, args);
+            pageDefinition.Renderings.AddRange(renderings);
+        }
+
+        public override void Process([NotNull] BuildPageDefinitionArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            var pageDefinition = args.Result;
+
+            if (pageDefinition == null)
+            {
+                return;
+            }
+
+            this.AddRenderings(pageDefinition, args);
+        }
+    }
+}
